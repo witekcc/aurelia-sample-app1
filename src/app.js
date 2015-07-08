abea60fb7d2a5c0c852f097b53129f9ec6f83535
wciemiega <wciemiega@permissiondata.com>
2015-07-08 10:13:33 -0400

    Added recursive child router

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
     config.map([
-      { route: ['','welcome'], name: 'welcome',  moduleId: './welcome',      nav: true, title:'Welcome' },
-      { route: 'flickr',       name: 'flickr',   moduleId: './flickr',       nav: true, title:'Flickr' }
+      { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
+      { route: 'flickr',       name: 'flickr',       moduleId: './flickr',       nav: true, title:'Flicker' },
+      { route: 'child-router', name: 'childRouter',  moduleId: './child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
